2745f8858d2099facc22d41c7c7432554c9f9490
YT-NiteTalon <131918235+YT-NiteTalon@users.noreply.github.com>
2023-04-26 13:59:29 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+namespace SpriteKind {
+    export const MrBeanz = SpriteKind.create()
+}
+tiles.setCurrentTilemap(tilemap`level2`)
+let MrBeanz = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . 1 1 f f f f . . . . . . 
+    . . . 1 1 f 1 1 f f . . . . . . 
+    . . f 1 f f f 1 1 f f f . . . . 
+    . . f 1 f f f f 1 f f f . . . . 
+    . . . f 1 1 f f 1 f f f . . . . 
+    . . . f f f 1 1 1 f f f . . . . 
+    . . . f f f f 1 1 f f f f . . . 
+    . . . f f f f f 1 f f f . . . . 
+    . . . f f f f 1 1 f f f . . . . 
+    . . . f f f 1 1 1 f f . . . . . 
+    . . . . f f 1 1 f f . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.MrBeanz)
+forever(function () {
+	
+})
